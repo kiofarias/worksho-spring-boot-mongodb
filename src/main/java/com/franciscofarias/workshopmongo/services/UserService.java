e496277970657ab20d48872f272ce89c07dd2caf
Francisco Farias <kiofarias@gmail.com>
2019-03-06 10:17:19 -0300

    Deleção de usuário com DELETE

--- src/main/java/com/franciscofarias/workshopmongo/services/UserService.java
+++ src/main/java/com/franciscofarias/workshopmongo/services/UserService.java
@@ -33,6 +33,10 @@ public class UserService {
 		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 	
+	public void delete(String id) {
+		findById(id);
+		repo.deleteById(id);
+	}
 	public User insert(User obj) {
 		return repo.insert(obj);
 	}
